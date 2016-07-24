efa4f3dd4a5ec6866ae750ededcee2cf5c174362
Kevin M Granger <github@kevinmgranger.me>
2016-07-24 17:02:42 -0400

    Add rudimentary text entry

---
+++ Assets/Scripts/InputFieldModifier.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class TextSubmitEvent : UnityEvent<string> { }
+
+/// <summary>
+/// Listen for Returns.
+/// Note that this makes desktop-centric assumptions.
+/// This is kinda hackish. Maybe revisit.
+/// </summary>
+[RequireComponent(typeof(InputField))]
+public class InputFieldModifier : MonoBehaviour
+{
+    public InputField inputField;
+
+    public TextSubmitEvent onReturn;
+
+    /// <summary>
+    /// Refocus on text field after submitting.
+    /// </summary>
+    public bool refocus;
+
+    void Reset()
+    {
+        inputField = GetComponent<InputField>();
+        inputField.lineType = InputField.LineType.MultiLineNewline;
+    }
+
+    void OnEnable()
+    {
+        inputField.onValueChanged.AddListener(this.OnValueChanged);
+    }
+
+    void Update()
+    {
+    }
+
+    void OnDisable()
+    {
+        inputField.onValueChanged.RemoveListener(this.OnValueChanged);
+    }
+
+    public void OnValueChanged(string value)
+    {
+        // contains instead of just checking the last one,
+        // to handle return being hit in the middle
+        if (value.Contains("\n"))
+        {
+            value = value.Replace("\n", "");
+            inputField.text = "";
+
+            if (value.Length == 0) return; // empty enter
+
+            inputField.DeactivateInputField();
+            Debug.Log(value, this);
+            onReturn.Invoke(value);
+            if (refocus) inputField.ActivateInputField();
+        }
+    }
+}
---
+++ Assets/Scripts/TextAppender.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppender : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+    }
+
+    void Reset()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+    }
+
+
+    public void AppendText(string text)
+    {
+        // TODO: not this, ever
+        if (text == "clear") this.text.text = "";
+        else this.text.text += "\n" + text + "\n"; // text
+    }
+}
